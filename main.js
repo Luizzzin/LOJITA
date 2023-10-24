c3daa59ff7c93c32944deb39605035b40c1b0da6
Luizzzin <luizhenribarbodias@gmail.com>
2023-10-24 08:37:22 -0300

    up
    up

--- main.js
+++ main.js
@@ -1,66 +1,78 @@
 const itensCarrinho = {}
 
-function addCarrinho(itemNome, itemPreco) {
-    if (itensCarrinho[itemNome]) {
+function addCarrinho(itemNome, itemPreco){
+    // Verifica se o item adicionado já existe no carrinho
+    if(itensCarrinho[itemNome]){
+        // A quantidade do produto vai aumentar
         itensCarrinho[itemNome].quantity++
         itensCarrinho[itemNome].precoTotal += itemPreco
         itensCarrinho[itemNome].liItem.querySelector(".quantity").innerHTML = itensCarrinho[itemNome].quantity;
-        itensCarrinho[itemNome].liItem.querySelector("preco-total").innerHTML = "R$" + itensCarrinho[itemNome].precoTotal.toFixed(2)
+        itensCarrinho[itemNome].liItem.querySelector(".preco-total").innerHTML = "R$" + itensCarrinho[itemNome].precoTotal.toFixed(2)
     } else {
         const liItem = document.createElement("li")
         liItem.innerHTML = `
         <div class="item">
-            <span>${itemNome}</span>       
-            <button class="add" onclick="addCarrinho ('${itemNome}', ${itemPreco})">+</button>
-            <span class="quantity>1</span>       
-            <button class="remove" onclick="removeCarrinho('${itemNome}', ${itemPreco}")>-</button>
-            <span class="precoTotal">R$${itemPreco.toFixed(2)}</span>
-        </div>
+            <span>${itemNome}</span>
+            <button class="remove" onclick="removeCarrinho('${itemNome}', ${itemPreco})">-</button>
+            <span class="quantity">1</span>
+            <button class="add" onclick="addCarrinho('${itemNome}', ${itemPreco})">+</button>
+            <span class="preco-total">R$${itemPreco.toFixed(2)}</span>
+        </div>  
         `
+        
         document.getElementById("itens-lista").appendChild(liItem)
         itensCarrinho[itemNome] = {
             quantity: 1,
-            precoTotal:itemPreco,
+            precoTotal: itemPreco,
             liItem: liItem
         }
     }
-    let precoTotal = 0 
-    for(let itemNome in itensCarrinho){
+    // calcula o valor total
+    let precoTotal = 0
+    for(let itemNome in itensCarrinho) {
         precoTotal += itensCarrinho[itemNome].precoTotal
     }
+
+    // atualiza o valor total
     document.getElementById("preco-total").innerHTML = "Valor Total R$"+precoTotal.toFixed(2)
     updateCarrinho()
 }
-function removeCarrinho(itemNome, itemPreco){
-    if(itensCarrinho[itemNome]){
-        if(itensCarrinho[itemNome].quantity>1){
+
+function removeCarrinho(itemNome, itemPreco) {
+    if(itensCarrinho[itemNome]) {
+        if(itensCarrinho[itemNome].quantity > 1){
             itensCarrinho[itemNome].quantity--
             itensCarrinho[itemNome].precoTotal -= itemPreco
-            itensCarrinho[itemNome].liItem.querySelector(".quantity").innerHTML=itensCarrinho[itemNome].quantity
+            itensCarrinho[itemNome].liItem.querySelector(".quantity").innerHTML = itensCarrinho[itemNome].quantity
             itensCarrinho[itemNome].liItem.querySelector(".preco-total").innerHTML = "R$" + itensCarrinho[itemNome].precoTotal.toFixed(2)
-        } else{
+        } else {
             document.getElementById("itens-lista").removeChild(itensCarrinho[itemNome].liItem)
             delete itensCarrinho[itemNome]
+            document.getElementById("preco-total").innerHTML = "Valor Total: R$ 0.00" 
         }
         updateCarrinho()
-        document.getElementById("preco-total").innerHTML = "Valor Total: R$"+ precoTotal.toFixed(2)
+        document.getElementById("preco-total").innerHTML = "Valor Total: R$" + precoTotal.toFixed(2)
         updateCarrinho()
     }
 }
-function updateCarrinho(){
+function updateCarrinho() {
+    
+}
+
+function limparCarrinho() {
     document.getElementById("itens-lista").innerHTML = ""
-    document.getElementById("preco-total").innerHTML = "Valor Total R$ 0.00"
-    for(let itemNome in itensCarrinho){
+    document.getElementById("preco-total").innerHTML = "Valor Total R$0.00"
+    for(let itemNome in itensCarrinho) {
         delete itensCarrinho[itemNome]
     }
     updateCarrinho()
 }
+
 function toggleCarrinho(){
     const itensCarrinhoDiv = document.getElementById("carrinho-itens")
-    if (itensCarrinhoDiv.style.display = "none"){
+    if (itensCarrinhoDiv.style.display == "none") {
         itensCarrinhoDiv.style.display = "block"
-
-    } else{
+    } else {
         itensCarrinhoDiv.style.display = "none"
     }
 }
